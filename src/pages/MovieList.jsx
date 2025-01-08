dba2dfb09c290da45572a6237d7c988b794aa02f
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2025-01-07 19:49:54 -0700

    configured tailwind

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export const MovieList = () => {
-  return <main>Movie List yeah</main>;
+  return (
+    <main>
+      <div className="text-5xl">Movie List yeah</div>
+
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+      </div>
+    </main>
+  );
 };
