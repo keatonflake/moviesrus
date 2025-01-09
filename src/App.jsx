b04a918fa3f8d4efb98aa07a654b489eb6dd14e3
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2025-01-08 21:03:15 -0700

    added basic header footer navigation and tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025{" "}
+          <Link to="/">
+            <a className="hover:underline">Movies-R-Us</a>
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link target="blank" to="https://www.linkedin.com/in/keaton-flake/">
+              <a className="hover:underline me-4 md:me-6">LinkedIn</a>
+            </Link>
+          </li>
+          <li>
+            <Link target="blank" to="https://github.com/keatonflake">
+              <a className="hover:underline me-4 md:me-6">GitHub</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
