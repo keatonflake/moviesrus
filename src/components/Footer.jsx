9edeb2406dce4b3cf875319dba74f4eb89582dd2
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2025-01-09 21:49:05 -0700

    added functional header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,24 +2,32 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className="bg-white shadow dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2025{" "}
-          <Link to="/">
-            <a className="hover:underline">Movies-R-Us</a>
+          <Link to="/" className="hover:underline">
+            Movies-R-Us
           </Link>
           . All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <Link target="blank" to="https://www.linkedin.com/in/keaton-flake/">
-              <a className="hover:underline me-4 md:me-6">LinkedIn</a>
+            <Link
+              target="_blank"
+              to="https://www.linkedin.com/in/keaton-flake/"
+              className="hover:underline me-4 md:me-6"
+            >
+              LinkedIn
             </Link>
           </li>
           <li>
-            <Link target="blank" to="https://github.com/keatonflake">
-              <a className="hover:underline me-4 md:me-6">GitHub</a>
+            <Link
+              target="_blank"
+              to="https://github.com/keatonflake"
+              className="hover:underline me-4 md:me-6"
+            >
+              GitHub
             </Link>
           </li>
         </ul>
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -4,10 +4,6 @@ export const MovieList = () => {
   return (
     <main>
       <div className="text-5xl">Movie List yeah</div>
-
-      <div className="bg-blue-500 text-white p-4">
-        <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
-      </div>
     </main>
   );
 };
