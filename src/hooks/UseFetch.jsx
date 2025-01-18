a252db1ccbcd3e6e2e42c60a6ae37f912ee2d752
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2025-01-17 21:59:08 -0700

    added search

--- src/hooks/UseFetch.jsx
+++ src/hooks/UseFetch.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 
-export const UseFetch = (apiPath) => {
+export const UseFetch = (apiPath, queryTerm = "") => {
   const [data, setData] = useState([]);
   const apiKey = import.meta.env.VITE_TMDB_API_KEY;
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${apiKey}`;
+  const url = queryTerm
+    ? `https://api.themoviedb.org/3/${apiPath}?api_key=${apiKey}&query=${queryTerm}`
+    : `https://api.themoviedb.org/3/${apiPath}?api_key=${apiKey}`;
 
   useEffect(() => {
     async function fetchMovies() {
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { UseFetch } from "../hooks/UseFetch";
+import { MovieCard } from "../components/MovieCard";
 
-export const Search = () => {
-  return <main>Search Results</main>;
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryTerm = searchParams.get("q");
+  const { data: movies } = UseFetch(apiPath, queryTerm);
+
+  return (
+    <main>
+      <section className="py-7">
+        <p className="text-3xl text-gray-700 dark:text-white">
+          {movies.length === 0
+            ? `No result found for '${queryTerm}'`
+            : `Result for '${queryTerm}'`}
+        </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
