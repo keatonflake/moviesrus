228a7221edd04a7e9064ed63811ab0e1ae6c30b1
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2025-01-25 20:12:27 -0700

    fixed vite host/render deploy issue

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: import.meta.env.VITE_PORT || 5173,
+  },
 });
